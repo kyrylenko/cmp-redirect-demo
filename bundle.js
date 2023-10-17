a7babba98403215bde9341bb6fca4d46b3f62fd3
Pavlo <pavlo@id-ward.com>
2023-10-17 14:23:45 +0300

    corrected redirect

--- bundle.js
+++ bundle.js
@@ -2,7 +2,10 @@ window.__tcfapi('addEventListener', 2, (tcData, success) => {
     console.log(tcData, success)
 
     if (success && tcData.eventStatus === 'useractioncomplete') {
+        console.log('curr href is', window.location.href)
+        const href = window.location.href
         //redirect to the /acknowledgement page
-        window.location.href = '/acknowledgement'
+        window.location.href = `${href}acknowledgement`
+        //window.history.pushState({}, '', '/acknowledgement')
     }
 })
