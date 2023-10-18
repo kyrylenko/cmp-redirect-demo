4105f8d5edc93d5971dc9acc8460bdac58ac15fb
Pavlo <pavlo@id-ward.com>
2023-10-18 13:42:53 +0300

    styling

--- bundle.js
+++ bundle.js
@@ -2,8 +2,10 @@ window.__tcfapi('addEventListener', 2, (tcData, success) => {
     console.log(tcData, success)
 
     if (success && tcData.eventStatus === 'useractioncomplete') {
-        window.location.href = 'https://id-ward.github.io/cmp-redirect-demo'
-        //window.location.href = `${location.href}acknowledgement`
+        //window.location.href = 'https://id-ward.github.io/cmp-redirect-demo'
+        window.location.href = location.hostname === 'localhost'
+            ? `${location.href}acknowledgement`
+            : 'https://id-ward.github.io/cmp-redirect-demo'
         //window.history.pushState({}, '', '/acknowledgement')
     }
 })
