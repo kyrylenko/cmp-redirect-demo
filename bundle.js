4e422371524aaf48f2242546c4f721846844e688
Pavlo <pavlo@id-ward.com>
2023-10-18 13:21:42 +0300

    scripts adopted

--- bundle.js
+++ bundle.js
@@ -3,6 +3,7 @@ window.__tcfapi('addEventListener', 2, (tcData, success) => {
 
     if (success && tcData.eventStatus === 'useractioncomplete') {
         window.location.href = 'https://id-ward.github.io/cmp-redirect-demo'
+        //window.location.href = `${location.href}acknowledgement`
         //window.history.pushState({}, '', '/acknowledgement')
     }
 })
